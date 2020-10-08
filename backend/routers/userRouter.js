8b4002369ad7b47ebf6fc2e62fa00c79fa6fcdf7
Karolis Žilevičius <karolis.zilevicius@gmail.com>
2020-10-08 20:22:03 +0300

    signin api

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -2,11 +2,31 @@ import express from 'express';
 import data from '../data.js';
 import User from '../models/userModel.js';
 import expressAsyncHandler from 'express-async-handler';
+import bcrypt from 'bcryptjs';
+import { generateToken } from '../utils.js';
+
 const userRouter = express.Router();
 userRouter.get('/seed', expressAsyncHandler(async (req, res)=>{
-    await User.remove({});
+//    await User.remove({});
 const createdUsers = await User.insertMany(data.users)
 res.send({createdUsers});
+}));
+
+userRouter.post('/signin', expressAsyncHandler(async(req, res)=>{
+    const user = await User.findOne({email: req.body.email});
+    if(user){
+        if(bcrypt.compareSync(req.body.pw, user.pw)){
+            res.send({
+                _id: user._id,
+                nick: user.nick,
+                email: user.email,
+                admin: user.admin,
+                token: generateToken(user)
+            });
+            return;
+        }
+    }
+    res.status(401).send({message: 'Blogas el. paštas arba slaptažodis'})
 }));
 
 export default userRouter
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 
+
+dotenv.config();
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/newshop',{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
 const app = express();
+//tarpininkas(middleware)
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/api/users', userRouter);
 
