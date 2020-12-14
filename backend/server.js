b33db13acbc6389b2649be1b104fce973440f4b5
Karolis Žilevičius <karolis.zilevicius@gmail.com>
2020-12-14 20:48:21 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.get('/', (req, res)=>{
     res.send('Serveris užsikūrė');
 });
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
     res.status(500).send({message: err.message});
 });
 const port = process.env.PORT || 5000;
